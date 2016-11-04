6947e9ee7bc3989046ba54403904f478a21fa432
Vasi Uiuiu <Vasi Uiuiu>
2016-11-04 20:51:35 +0200

    Done..

--- src/PersonAgenda.java
+++ src/PersonAgenda.java
@@ -1,30 +1,37 @@
 public class PersonAgenda {
 
-    static Person[] listName = new Person[2]; // store the names
-    static int index=0;
+    static Person[] listName = new Person[8]; // store the names
+    static int index = 0;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int option=0;
+        int option = 0;
 
         do {
 
             // afisare meniu
-            System.out.println("1.fa aia");
+            System.out.println("1.Adauga contact.");
             System.out.println("1.fa ailalta");
 
 
             //selectare optiune
-            option=SkeletonJava.readIntConsole("Option:");
+            option = SkeletonJava.readIntConsole("Option:");
 
             switch (option) {
-                case 1: add();break;
-                case 2: print();break;
+                case 1:
+                    add();
+                    break;
+                case 2:
+                    print();
+                    break;
+                case 3:
+                    del();
+                    break;
+
             }
 
 
         }
-        while(option!=4);
+        while (option != 5);
 
     }
 
@@ -34,40 +41,73 @@ public class PersonAgenda {
 
         // de optimizat daca deja exista o pers cu acel nume
 
-        if(index<listName.length) {
-            String name = SkeletonJava.readStringConsole("Input the name:");
-            String number = SkeletonJava.readStringConsole("Input the number:");
+        if (index < listName.length) {
+            String name = SkeletonJava.readStringConsole("Introduceti nume contact: ");
+            String number = SkeletonJava.readStringConsole("Introduceti numar de telefon: ");
 
-            Person p = new Person();
-            p.name=name;
-            p.phoneNumber=number;
-            listName[index] = p;
-            index++;
-        }
-        else
-        {
+
+        } else {
             System.out.println("gata memoria, schimba telul ");
         }
     }
 
     static void print() {
+        System.out.println();
+        for (int i = 0; i <= index; i++) {
+            System.out.println(listName[i]);
+        }
+
 
     }
 
     static void del() {
 
         // citeste un nume
+        String name = SkeletonJava.readStringConsole("Introduceti nume: ");
+        String numer = SkeletonJava.readStringConsole("Introduce numar de telefon");
         // il cauta in array
-        //daca il gaseste il sterge  asa listName[unde l-a gasit]=null sau "";
+        for (int i = 0; i <= index; i++) {
+            if (name.equals(listName[i])) {
 
+
+                //daca il gaseste il sterge  asa listName[unde l-a gasit]=null sau "";
+
+
+
+
+                    // citeste un nume
+                    // il cauta in array
+                    // daca il gaseste cere noul nume si il substituie pe cel vechi cu cel nou
+
+
+                    Person persoana1 = new Person();
+                    persoana1.name = "Vasile";
+                    persoana1.phoneNumber = "0744987367";
+                    listName[index] = persoana1;
+
+
+                    Person persoana2 = new Person();
+                    persoana2.name = "Beniamin";
+                    persoana2.phoneNumber = "0743987367";
+                    listName[index] = persoana2;
+
+                    Person peroana3 = new Person();
+                    peroana3.name = "Leon";
+                    peroana3.phoneNumber = "0764954282";
+                    listName[index] = peroana3;
+
+                    Person persoana4 = new Person();
+                    persoana4.name = "Darius";
+                    persoana4.phoneNumber = "0762258250";
+                    listName[index] = persoana4;
+                }
+            }
+        }
     }
 
-    static void modify() {
-
-        // citeste un nume
-        // il cauta in array
-        // daca il gaseste cere noul nume si il substituie pe cel vechi cu cel nou
-    }
 
 
-}
+
+
+
+
--- src/SimpleAgenda.java
+++ src/SimpleAgenda.java
@@ -1,44 +1,94 @@
 public class SimpleAgenda {
 
-    static String[] listName = new String[2]; // store the names
+    static String[] listName = new String[7]; // store the names
     static int index=0;
 
+
+    static void initializare(){
+        listName[index] = "Beniamin";
+
+        index++;
+        listName[index] = "Vasile";
+
+        index++;
+        listName[index] = "Sofia";
+
+        index++;
+        listName[index] = "Peuu";
+
+        index++;
+        listName[index] = "Abigail";
+
+         index++;
+        listName[index] = "Petru ";
+
+         index++;
+        listName[index] = "Leon";
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         int option=0;
-
+        initializare();
         do {
-
             // afisare meniu
-            System.out.println("1.fa aia");
-            System.out.println("1.fa ailalta");
+            System.out.println("1.Contact nou.");
+            System.out.println("2.Toate contactele.");
+            System.out.println("3.Sterge contact.");
+            System.out.println("4.Modifica contact.");
 
 
             //selectare optiune
             option=SkeletonJava.readIntConsole("Option:");
 
+            System.out.println();
+
             switch (option) {
-                case 1: add();break;
-                case 2: print();break;
+                case 1: add();
+                    break;
+                case 2: print();
+                    break;
+                case 3: del();
+                    break;
+                case 4: modify();
+                    break;
             }
 
 
         }
-        while(option!=4);
+        while(option!=5);
 
+    }
+
+    static boolean cautaNume(String nume){
+        for(int i=0; i <= index; i++){
+            if(listName[i].equals(nume)){
+                return true;
+            }
+        }
+        return false;
     }
 
     static void add() {
 
         // de optimizat cand se umple agenda
+        if(index == listName.length){
 
-        // de optimizat daca deja exista o pers cu acel nume
-
-        if(index<listName.length) {
-            String name = SkeletonJava.readStringConsole("Input the name:");
-            listName[index] = name;
-            index++;
         }
+
+
+        if(index < listName.length) {
+            String name = SkeletonJava.readStringConsole("Introduceti nume:");
+
+            // de optimizat daca deja exista o pers cu acel nume
+            if(!cautaNume(name)) {
+                index++;
+                listName[index] = name;
+                index++;
+            }
+        }
+
         else
         {
             System.out.println("gata memoria, schimba telul ");
@@ -46,23 +96,40 @@ public class SimpleAgenda {
     }
 
     static void print() {
-
+        System.out.println("======= TOATE CONTACTELE =======");
+        for(int i=0; i<= index; i++){
+            System.out.println(listName[i]);
+        }
+        System.out.println("================================");
+        System.out.println();
     }
 
     static void del() {
 
         // citeste un nume
+        String name = SkeletonJava.readStringConsole("Introduceti nume: ");
         // il cauta in array
-        //daca il gaseste il sterge  asa listName[unde l-a gasit]=null sau "";
+
+        for(int i=0; i<= index; i++) {
+            if (name.equals(listName[i])) {
+                //daca il gaseste il sterge  asa listName[unde l-a gasit]=null sau "";
+                listName[i] = "";
+            }
+        }
 
     }
 
-    static void modify() {
+    static void modify( ) {
 
         // citeste un nume
+        String numeVechi = SkeletonJava.readStringConsole("Introduceti nume care doriti sa il inlocuiti: ");
+        String numeNou = SkeletonJava.readStringConsole("Introduceti numele nou: ");
         // il cauta in array
-        // daca il gaseste cere noul nume si il substituie pe cel vechi cu cel nou
+        for(int i=0; i<= index; i++) {
+            if (numeVechi.equals(listName[i])) {
+                // daca il gaseste cere noul nume si il substituie pe cel vechi cu cel nou
+
+            }
+        }
     }
-
-
 }
